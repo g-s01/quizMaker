c87eb5b6d7a4009a2e09dede87df65e5c5027806
Gautam Sharma <gautam_sharma1@yahoo.com>
2023-06-22 11:47:49 +0530

    registration api made

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+app.use(express.json());
 const dbConfig = require('./config/dbConfig');
+
+const userRoute = require("./routes/usersRoutes");
+app.use("/api/users", userRoute);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
